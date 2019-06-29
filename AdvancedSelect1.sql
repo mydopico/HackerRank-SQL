d734387401cb5c2cd6b4267ca0b4a08168239da8
mydopico <mydopico@gmail.com>
2019-06-29 17:03:35 +0200

    Create AdvancedSelect1.sql

---
+++ AdvancedSelect1.sql
@@ -0,0 +1,27 @@
+--HACKERRANK - SQL
+
+--ADVANCED SELECT 1
+
+--Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. Output one of the following statements for each record in the table:
+
+--Equilateral: It's a triangle with sides of equal length.
+--Isosceles: It's a triangle with sides of equal length.
+--Scalene: It's a triangle with sides of differing lengths.
+--Not A Triangle: The given values of A, B, and C don't form a triangle.
+
+--TRIANGLES table
+
+--Column       Type
+--A            Integer
+--B            Integer
+--C            Integer
+
+
+SELECT 
+CASE 
+  WHEN A + B <= C OR A + C <= B OR B + C <= A THEN 'Not A Triangle' 
+  WHEN A=B AND B=C THEN 'Equilateral' 
+  WHEN A=B OR A=C OR B=C THEN 'Isosceles' 
+  ELSE 'Scalene' 
+END 
+FROM TRIANGLES;
